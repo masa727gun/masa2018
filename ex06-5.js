0740e91751d6e8085c7140f911339f1e1a9036a7
g.masaaki727 <g.masaaki727@gmail.com>
2018-10-21 21:52:56 -0400

    sam

--- ex06-5.js
+++ ex06-5.js
@@ -1,3 +1,13 @@
-function sayhello65(){var who65='world';alert('Hello,'+who65+'!');}
-function taro65(){who65='太郎';}
-function hanako65(){who65='花子';}
+var who65 = 'world';
+// var come = 'が来た'
+function sayhello65() {
+// var who = 'world';
+  alert('Hello, ' + who65 + '!');
+} 
+
+function taro65() {
+  who65 = '太郎' ;
+} 
+function hanako65() {
+  who65 = '花子';
+}
--- ex06-7.js
+++ ex06-7.js
@@ -1,2 +1,7 @@
-var who='world';function sayhello67(){alert('Hello,'+who+'!');}
-function someone(x){who=x;}
+var who = 'world';
+function sayhello67() {
+  alert('Hello, ' + who + '!');
+}
+function someone(x) {
+  who = x;
+}  
